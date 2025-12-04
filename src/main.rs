b0ac4cfb252b4b5c46ae1471db43e19e113a8113
Parrot Live user <user@localhost.localdomain>
2025-12-04 11:07:53 +0530

    some refactoring

--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut result = scrape_remoteok(&mut seen_urls).await?;
     jobs.extend(result);
     thread::sleep(Duration::from_secs(2));
-    println!("Scarping we work remotely.................");
-    result = scrape_weworkremotely(&mut seen_urls).await?;
-    jobs.extend(result);
+    println!("Scraping completed.................");
     let json = serde_json::to_string_pretty(&jobs)?;
     let mut file = File::create("remote_jobs.json").expect("could not create file");
     file.write_all(json.as_bytes())?;
@@ -48,18 +46,15 @@ async fn scrape_remoteok(seen: &mut HashSet<String>) -> Result<Vec<Job>, Box<dyn
                     let salary_max = job["salary_max"].as_str().unwrap_or("NA").to_string();
                     let tags:Vec<String> = job["tags"]
                     .as_array()
-                    .and_then(|row| row.get(tag_index))
-                    .and_then(|v| v.as_array())
                     .unwrap_or(&vec![])
                     .iter()
                     .filter_map(|v| v.as_str().map(String::from))
                     .collect();
                     let url = job["apply_url"].as_str().unwrap_or("NA").to_string();
-                    let posted = Utc.timestamp_opt(job["epoch"].as_i64().unwrap(),0)
-                                    .single()
-                                    .unwrap()
-                                    .format("%Y-%m-%d")
-                                    .to_string();
+                    let posted =  job["epoch"].as_i64()
+                                    .and_then(|epoch| Utc.timestamp_opt(epoch,0).single())
+                                    .map(|dt| dt.format("%Y-%m-%d").to_string())
+                                    .unwrap_or_else(|| "NA".to_string());
 
 
                     let my_job = Job{
@@ -73,10 +68,6 @@ async fn scrape_remoteok(seen: &mut HashSet<String>) -> Result<Vec<Job>, Box<dyn
                            posted:posted.clone(),
                         };
                     result.push(my_job);
-                    println!("{}",title);
                 }
            Ok(result)
     }
-async fn scrape_weworkremotely(seen: &mut HashSet<String>) -> Result<Vec<Job>, Box<dyn std::error::Error>>{
-            Ok(Vec::new())
-         }
